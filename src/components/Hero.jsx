1c646eaf0ed0d7b6f9e7f7372247bd177cd92b34
Dhirajverm1480 <dhirajv418@gmail.com>
2026-03-05 15:13:41 +0530

    Home Page improve

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,22 @@
 export const Hero = () => {
   return (
-      <div className="absolute inset-0 sm:-top-14 -z-10 bg-[#dddff8] overflow-hidden">
-        <video
-          src="https://framerusercontent.com/assets/bVq4NvV5SkQBR0D7VqD3K4BXIdM.mp4"
-          autoPlay
-          loop
-          muted
-          playsInline
-          poster="/fallback.jpg"
-          // preload="auto"
-          preload="metadata"
-          aria-hidden="true"
-          tabIndex={-1}
-          disablePictureInPicture
-          className="w-full h-[60%] md:h-full object-cover pointer-events-none"
-        />
-      </div>
+    <div className="absolute inset-0 sm:-top-14 -z-10 bg-[#dddff8]">
+      <video
+        src="https://framerusercontent.com/assets/bVq4NvV5SkQBR0D7VqD3K4BXIdM.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        poster="/fallback.jpg"
+        // preload="auto"
+        preload="metadata"
+        aria-hidden="true"
+        tabIndex={-1}
+        disablePictureInPicture
+        className="absolute inset-0 w-full h-[80%] md:h-full object-cover object-top pointer-events-none"
+      />
+      {/* Blend Gradient */}
+      <div className="absolute bottom-[18%] md:bottom-0 left-0 w-full h-40 bg-gradient-to-b from-transparent to-[#dddff8]" />
+    </div>
   );
 };
